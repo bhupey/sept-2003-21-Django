079984124f4a02eacf9641099bf0d17f7491f564
bhupey <bhupendra.neupane08@gmail.com>
2023-10-05 11:34:21 +0545

    crud ClassRoom

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
 
 ]
 
-PROJECT_APPS = ['myapp', 'temp_inheritance', "static_render","temp_forms"]
+PROJECT_APPS = ['myapp', 'temp_inheritance', "static_render","temp_forms", "crud"]
 THIRD_PARTY_APPS = ['django_extensions']
 INSTALLED_APPS += PROJECT_APPS + THIRD_PARTY_APPS
 
--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('crud.urls')),
     path('', include('myapp.urls')),
     path('forms', include("temp_forms.urls")),
     path('inherit/', include('temp_inheritance.urls')),
